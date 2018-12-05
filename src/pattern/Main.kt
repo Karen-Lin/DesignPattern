3fe0924c8491d228783c9ec114b59492425987b0
Karen Lin <klin@health2sync.com>
2018-12-06 00:35:33 +0800

    Init builder pattern.

--- src/pattern/Main.kt
+++ src/pattern/Main.kt
@@ -1,5 +1,17 @@
 package pattern
 
+import pattern.builder.People
+
 fun main(args: Array<String>) {
+
     println("Design pattern study group.")
+
+    /** Builder pattern **/
+    val people: People =
+        People.PeopleBuilder("Karen", "Lin")
+            .nickName("Taiwan Top Model")
+            .address("Taipei Neihu")
+            .phone("0988333333")
+            .build()
+
 }
---
+++ src/pattern/builder/People.kt
@@ -0,0 +1,45 @@
+package pattern.builder
+
+
+class People private constructor(builder: PeopleBuilder) {
+
+    val firstName: String
+    val lastName: String
+    val nickName: String?
+    val phone: String?
+    val address: String?
+
+    init {
+        this.firstName = builder.firstName
+        this.lastName = builder.lastName
+        this.nickName = builder.nickName
+        this.phone = builder.phone
+        this.address = builder.address
+    }
+
+    class PeopleBuilder(val firstName: String, val lastName: String) {
+        var nickName: String? = null
+        var address: String? = null
+        var phone: String? = null
+
+
+        fun nickName(nickName: String): PeopleBuilder {
+            this.nickName = nickName
+            return this
+        }
+
+        fun phone(phone: String): PeopleBuilder {
+            this.phone = phone
+            return this
+        }
+
+        fun address(address: String): PeopleBuilder {
+            this.address = address
+            return this
+        }
+
+        fun build(): People {
+            return People(this)
+        }
+    }
+}
