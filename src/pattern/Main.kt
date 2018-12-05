90e5c09a0fc9d29f5c22889b3aba782058041932
Karen Lin <klin@health2sync.com>
2018-12-06 00:42:07 +0800

    Init builder pattern.

--- src/pattern/Main.kt
+++ src/pattern/Main.kt
@@ -1,5 +1,12 @@
 package pattern
 
+import pattern.builder.People
+
 fun main(args: Array<String>) {
+
     println("Design pattern study group.")
+
+    /** Builder pattern **/
+    val people = People("Karen", "Lin", "Taiwan Top Model", "Taipei Neihu", "0988333333")
+
 }
---
+++ src/pattern/builder/People.kt
@@ -0,0 +1,3 @@
+package pattern.builder
+
+class People(val firstName: String, val lastName: String, val nickName: String, val address: String, val phone: String)
