ec8b8450832e9e034c8300aff31e42c1475dada8
Amin Hong <iampeymin@gmail.com>
2019-01-23 14:13:36 +0800

    Add command pattern.

--- src/pattern/command/Command.kt
+++ src/pattern/command/Command.kt
@@ -6,31 +6,21 @@ interface Command {
     fun move()
 }
 
-class JumpCommand(private val receiver: Receiver) : Command {
-    override fun move() = println("${receiver.name} jumping ^")
+class JumpCommand(private val receiver: Receiver) {
+    // println("${receiver.name} jumping ^")
 }
 
-class SwimCommand(private val receiver: Receiver) : Command {
-    override fun move() = println("${receiver.name} swimming ~")
+class SwimCommand(private val receiver: Receiver) {
+    // println("${receiver.name} swimming ~")
 }
 
-class WalkCommand(private val receiver: Receiver) : Command {
-    override fun move() = println("${receiver.name} walking -")
+class WalkCommand(private val receiver: Receiver) {
+    // println("${receiver.name} walking -")
 }
 
-class FlyCommand(private val receiver: Receiver) : Command {
-    override fun move() = println("${receiver.name} flying -===")
+class FlyCommand(private val receiver: Receiver) {
+    // println("${receiver.name} flying -===")
 }
 
-class MircoCommand : Command {
-
-    private val commandList = ArrayList<Command>()
-
-    fun addCommand(command: Command) {
-        commandList.add(command)
-    }
-
-    override fun move() {
-        commandList.forEach { it.move() }
-    }
+class MircoCommand {
 }
