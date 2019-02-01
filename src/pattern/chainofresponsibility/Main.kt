542a5fe332de1649db8031ecfec152c400921316
chiaen <little.zombie.10307276@gmail.com>
2019-02-01 11:49:17 +0800

    Change chain of responsibility example

--- src/pattern/chainofresponsibility/Main.kt
+++ src/pattern/chainofresponsibility/Main.kt
@@ -1,58 +1,29 @@
 package pattern.chainofresponsibility
 
-import pattern.chainofresponsibility.`object`.*
-import pattern.chainofresponsibility.enums.PostType
+import pattern.chainofresponsibility.request.LeaveRequest
 
 fun main(args: Array<String>) {
 
-    // 處理明信片
-    handlePostObject(PostCard())
+    val chiaen = LeaveRequest("佳恩", "感冒看醫生", 1)
+    handleLeaveRequest(chiaen)
 
-    // 處理信件
-    handlePostObject(Mail())
+    val karen = LeaveRequest("凱倫", "出國去日本玩", 5)
+    handleLeaveRequest(karen)
 
-    // 處理包裹
-    handlePostObject(Box())
+    val claire = LeaveRequest("克萊爾", "產假", 90)
+    handleLeaveRequest(claire)
 
-    // 處理米粒
-    handlePostObject(Rice())
+    val amin = LeaveRequest("阿敏", "心情不美麗", 180)
+    handleLeaveRequest(amin)
 
 
 }
 
-private fun handlePostObject(postPackage: PostPackage) {
-    val size = postPackage.length * postPackage.width * postPackage.height
-    when(postPackage.postType) {
-        PostType.POST_CARD -> {
-            val maxWeight = 0.18F //kg
-            val maxSize: Float = 14.8F * 10.5F * 1 //cm
-            val minSize: Float = 14F * 9F * 0.1F //cm
-            if (postPackage.weight <= maxWeight && (size > minSize && size <= maxSize)) {
-                println("明信片分類給郵差")
-            } else {
-                println("規格不符，退回")
-            }
-        }
-        PostType.MAIL -> {
-            val maxWeight = 2F //kg
-            val maxSize: Float = 60F * 60 * 60 //cm
-            val minSize: Float = 14F * 9F * 0.1F //cm
-            if (postPackage.weight <= maxWeight && (size > minSize && size <= maxSize)) {
-                println("信件分類給郵差")
-            } else {
-                println("規格不符，退回")
-            }
-        }
-        PostType.PACKAGE -> {
-            val maxWeight: Float = 20F //kg
-            val maxSum: Float = 150F //長+寬+高的總和 cm
-            val minSize: Float = 14F * 9F * 0.1F //cm
-            val boxSum = postPackage.length + postPackage.width + postPackage.height
-            if (postPackage.weight <= maxWeight && boxSum <= maxSum && size >= minSize) {
-                println("包裹分類給郵車")
-            } else {
-                println("規格不符，退回")
-            }
-        }
+private fun handleLeaveRequest(leaveRequest: LeaveRequest) {
+    when {
+        leaveRequest.day <= 3 -> println("小組長: 批准${leaveRequest.name}的${leaveRequest.day}天假")
+        leaveRequest.day <= 7 -> println("漢克: 批准${leaveRequest.name}的${leaveRequest.day}天假")
+        leaveRequest.day <= 90 -> println("愛德: 批准${leaveRequest.name}的${leaveRequest.day}天假")
+        else -> println("${leaveRequest.name}假單不被批准")
     }
 }
--- src/pattern/chainofresponsibility/enums/PostType.kt
+++
@@ -1,9 +0,0 @@
-package pattern.chainofresponsibility.enums
-
-enum class PostType {
-
-    POST_CARD,
-    MAIL,
-    PACKAGE
-
-}
